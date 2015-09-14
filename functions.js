44b02d3dc10d8bef5654aea2d628ee77dfdfb5d9
Catherine <ducharme.catherine@gmail.com>
2015-09-14 18:18:41 +0000

    Created a function that can be used with .sort() to sort an array of strings based on their length

--- functions.js
+++ functions.js
@@ -1,1 +1,19 @@
 //This is a line with a comment.
+
+//This function sorts strings based on their length.
+
+function compareLength(a, b) {
+    if (a.length < b.length) {
+        return -1;
+    }
+    if (a.length > b.length) {
+        return 1;
+    }
+    return 0;
+}
+
+//The code below tests the compareLength function.
+
+var testArray = ["medium", "sho", "zloooongest", "evenlongeeeeeeeer", "1"];
+
+testArray.sort(compareLength);
