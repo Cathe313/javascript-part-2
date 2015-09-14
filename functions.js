d514ef2ef1375774f024b2e850d98f0899f672c0
Catherine <ducharme.catherine@gmail.com>
2015-09-14 19:10:46 +0000

    Created code that can sort an array first by longest name, then by alphabetical order of email

--- functions.js
+++ functions.js
@@ -17,3 +17,41 @@ function compareLength(a, b) {
 var testArray = ["medium", "sho", "zloooongest", "evenlongeeeeeeeer", "1"];
 
 testArray.sort(compareLength);
+
+//Here is an array of objects:
+
+var addressBook = [ 
+    {name: "Brigitte", email:"brigitte@courrier.com"}, 
+    {name: "Ziad", email:"ziad@decodemtl.com"},
+    {name: "Jean-Jacques", email: "jj@test.com"},
+    {name: "Catherine", email: "catherine@gmail.com"},
+    {name: "Bob", email: "bob@gmail.com"}
+];
+
+//The code below will sort the above array by length of name.
+
+function compareAgain(a, b) {
+    if (a.name.length > b.name.length) {
+        return 1;        
+    }
+    if (a.length < b.length) {
+        return -1;
+    }
+    return 0;
+};
+
+addressBook.sort(compareAgain);
+
+//The fcode below sorts the addressBook array in alphabetical order of email address.
+
+function compareAlpha(a, b) {
+    if (a.email > b.email) {
+        return 1;        
+    }
+    if (a.length < b.length) {
+        return -1;
+    }
+    return 0;
+};
+
+addressBook.sort(compareAlpha);
