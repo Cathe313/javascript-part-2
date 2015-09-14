e93a46bee61ceef7ecc4b9ce4958113adf28b43e
Catherine <ducharme.catherine@gmail.com>
2015-09-14 23:45:19 +0000

    And again some more progress

--- terrain.js
+++ terrain.js
@@ -6,12 +6,12 @@ function Tile(x, y) {
         this.y = y;
         this.height = Math.floor(3*Math.random());
         //this.type
-            var randomType = (Math.floor(3*Math.random());
+            var randomType = (Math.floor(3*Math.random()))
             if (randomType === 0) {this.type = "grass"};
             if (randomType === 1) {this.type = "rocks"};
             if (randomType === 2) {this.type = "water"};
         //this.condition 
-            var randomCondition = (Math.floor(3*Math.random());
+            var randomCondition = (Math.floor(3*Math.random()));
             if (randomCondition === 0) {this.condition = "burning"};
             if (randomCondition === 1) {this.condition = "normal"};
             if (randomCondition === 2) {this.condition = "frozen"};
@@ -21,19 +21,32 @@ Tile.prototype = {
        freeze: function() {
             if (this.condition === "burning") {
                 this.condition = "normal";
+                return this.condition;
             } else if (this.condition === "normal") {
-                 this.condition === "frozen";
+                 this.condition = "frozen";
+                return this.condition
             } else {
                  this.height = this.height + 1;
-            }
+                return this.height;
+            } return this.height;
        }, 
        burn: function() {
             if (this.condition === "frozen") {
                 this.condition = "normal";
+                return this.condition;
             } else if (this.condition === "normal") {
-                this.condition === "burning";
+                this.condition = "burning";
+                return this.condition;
             } else if (this.condition === "burning" && this.height > 0) {
                 this.height = this.height - 1;
-            }
+                return this.height;
+            } return this.height;
        }
 }    
+ 
+//Testing with the following tile:
+
+var testTile = new Tile(3, 4);
+console.log(testTile.height);
+console.log(testTile.condition);
+testTile.burn();
